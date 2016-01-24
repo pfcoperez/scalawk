9b9e3b7b90c29fef1e8cf23e59b5ca802d4316da
Pablo Fco. Pérez Hidalgo <contact@pablofranciscoperez.info>
2016-01-24 22:20:46 +0100

    More steps implementing initial program state

--- src/main/scala/org/pfcoperez/scalawk/states/CommandWithInitialProgram.scala
+++ src/main/scala/org/pfcoperez/scalawk/states/CommandWithInitialProgram.scala
@@ -1,11 +1,20 @@
 package org.pfcoperez.scalawk.states
 
 import org.pfcoperez.scalawk.AwkCommand
-import org.pfcoperez.scalawk.entities.SideEffectStatement
+import org.pfcoperez.scalawk.entities.{AwkExpression, SideEffectStatement}
 import org.pfcoperez.scalawk.transitions.{ToCommandWithLineProgram, ToSolidCommand}
 
-class CommandWithInitialProgram(initialProgram: Seq[SideEffectStatement]) extends AwkCommand
+class CommandWithInitialProgram(initialProgram: Seq[SideEffectStatement])(prev: AwkCommand) extends AwkCommand
   with ToSolidCommand
   with ToCommandWithLineProgram {
+
+  override private[scalawk] val commandOptions: Seq[String] = prev.commandOptions
+  override private[scalawk] val linePresentation: Seq[AwkExpression] = prev.linePresentation
+  override private[scalawk] val lineProgram: Seq[SideEffectStatement] = prev.lineProgram
+
+  override private[scalawk] val initialProgram: Seq[SideEffectStatement] = prev.initialProgram
+
+  override private[scalawk] val endProgram: Seq[SideEffectStatement] = _
+  override private[scalawk] val fs: Option[String] = _
 
 }
--- src/main/scala/org/pfcoperez/scalawk/states/CommandWithSeparator.scala
+++ src/main/scala/org/pfcoperez/scalawk/states/CommandWithSeparator.scala
@@ -1,14 +1,15 @@
 package org.pfcoperez.scalawk.states
 
 import org.pfcoperez.scalawk.AwkCommand
-import org.pfcoperez.scalawk.transitions.{ToCommandWithLineProgram, ToSolidCommand}
+import org.pfcoperez.scalawk.transitions.{ToCommandWithInitialProgram, ToCommandWithLineProgram, ToSolidCommand}
 
 import scala.util.matching.Regex
 
 // https://www.gnu.org/software/gawk/manual/html_node/Default-Field-Splitting.html#Default-Field-Splitting
 class CommandWithSeparator(separator: Option[String] = None) extends AwkCommand
- with ToSolidCommand
- with ToCommandWithLineProgram {
+  with ToSolidCommand
+  with ToCommandWithLineProgram
+  with ToCommandWithInitialProgram {
 
   override val fs: Option[String] = separator
   override val commandOptions: Seq[String] = separator.map(sep => s"-F '$sep'").toSeq
